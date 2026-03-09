c29b6101b20b82889d061585870b191f4eec6424
Chloedona <chloeanndonaghue@gmail.com>
2026-03-09 19:09:36 +0000

    Planned in some functions and what they aim to do. Need to work on the code next

--- main_code.py
+++ main_code.py
@@ -7,7 +7,8 @@ import matplotlib.pylot as plt
 #I/O FUNCTIONS#
 def read_fasta(file)
     """
-    Reads a FASTA file
+    Reads a FASTA file and returns a dictionary
+    with header as key and sequence as a value
     """
     sequences = {}
 
@@ -27,16 +28,41 @@ def read_fasta(file)
                 seq.append(line)
         if name:
             sequences[name] = ''.join(seq)
-            
+
     return sequences
 
 #SEQUENCE ANALYSIS#
+def percent_identity(seq1, seq2):
+    """
+    Calculate the percent identity between two sequences
+    """
+    return
+def percent_difference(seq1, seq2):
+    """
+    Return the percentage difference between sequences
+    """
+    return
+def find_closest(mystery_seq, database):
+    """
+    Find the closest breed match from the database using the myserty sequence.
+    Return the breed name and similarity.
+    """
+    return 
 
-
-
-
+#STATISTICS#
+def p_value():
+    return
 
 #PHLOGENY#
+def build_tree():
+    """
+    Construct phylogenetic tree from mystery DNA and database
+    """
+
+def plot_tree():
+    """
+    Displays the tree to user
+    """
 
 
 
